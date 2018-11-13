c94c45f6f1e6c2a60b5b090619718abde357ed36
vutsalsinghal <vutsalsinghal@gmail.com>
2018-11-13 12:20:12 -0500

    Updates

---
+++ precache-manifest.1332522e452c8291710ba27196b84a4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c800570f84d0fe74749d",
+    "url": "/oingo/static/js/runtime~main.c800570f.js"
+  },
+  {
+    "revision": "6c6ccde62165d50fcfc5",
+    "url": "/oingo/static/js/main.6c6ccde6.chunk.js"
+  },
+  {
+    "revision": "5a762c93406f8b2899df",
+    "url": "/oingo/static/js/1.5a762c93.chunk.js"
+  },
+  {
+    "revision": "6c6ccde62165d50fcfc5",
+    "url": "/oingo/static/css/main.d3d12515.chunk.css"
+  },
+  {
+    "revision": "f876fb7e1e7b5f1d1a9e889d082965f9",
+    "url": "/oingo/index.html"
+  }
+];
